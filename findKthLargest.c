df3cb8fb53efe8ecd7a1d9760a559f32c91a4193
simon2 <bzy19921017@gmail.com>
2020-06-29 17:48:00 +0900

    add a solution by heap in findKthLargest.c

--- findKthLargest.c
+++ findKthLargest.c
@@ -33,7 +33,45 @@ int findKthLargest(int* nums, int numsSize, int k){
 /********* end of my naive solution *******/
 
 /********* better way using heap **********/
+void heapify(int* data, int index, int size){
+  int left = index * 2 + 1;
+  int right = index * 2 + 2;
+  int smallest;
+  if(left < size && data[left] < data[index]){
+    smallest = left;
+  }else{
+    smallest = index;
+  }
+  if(right < size && data[right] < data[smallest]){
+    smallest = right;
+  }
+  if(smallest != index){
+    int temp = data[index];
+    data[index] = data[smallest];
+    data[smallest] = temp;
+    heapify(data, smallest, size);
+  }
+}
 
+void build_heap(int* data, int size){
+  int i;
+  for(i=(size-1)/2;i>=0;i--){
+    heapify(data,i,size);
+  }
+}
+
+int findKthLargest(int* nums, int numsSize, int k){
+    build_heap(nums,k);
+    int i = k;
+    while(i < numsSize){
+        if(nums[i] > nums[0]){
+            nums[0] = nums[i];
+            heapify(nums,0,k);
+        }
+        i++;
+    }
+    return nums[0];
+}
 /********* end of heap solution ***********/
 
 /********* best way using quick select **********/
